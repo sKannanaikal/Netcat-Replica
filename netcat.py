129df01dcb75161b780ab93fd7221ba8b21da82a
sKannanaikal <stk5106@rit.edu>
2021-06-30 10:59:52 -0400

    continued development on the netcat replica

--- netcat.py
+++ netcat.py
@@ -1,9 +1,30 @@
 import socket
 import optparse
 
-def connect(target, port):
-	connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	connection.connect(target, port)
+class compSystem(self, target, port, data=None):
+	self.target = target
+	self.port = port
+	self.data = data
+	self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	def run(self):
+		if self.cmd_args.wait:
+			self.listen()
+		else:
+			self.send()
+
+	def sendCommand(self):
+		self.socket.connect((self.target, self.port))
+		if self.buffer is not None:
+			self.socket.send(self.data)
+		try:
+			bas
+		except:
+			bas
+
+
+def executeCMD(command):
+	command = ''
 
 def main():
 	command = optparse.OptionParser()
