848b77ecbc3d208505879e4022ce204bd9da9da2
sKannanaikal <stk5106@rit.edu>
2021-06-30 17:35:55 -0400

    continued work on netcat script

--- netcat.py
+++ netcat.py
@@ -3,53 +3,65 @@ import optparse
 import shlex
 import sys
 
-class compSystem(self, target, port, data=None):
+class compSystem(self, target, port):
 	self.target = target
 	self.port = port
-	self.data = data
 	self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 	def run(self):
 		self.sendCommand()
 
-	def sendCommand(self):
-		self.socket.connect((self.target, self.port))
-		if self.data is not None:
-			self.socket.send(self.data)
+	def sendCommand(self, command):
 		try:
-			while True:
-				data_size = 1
-				response = ''
-				while True:
-					data = self.socket.recv(4096)
-					response += data.decode()
-					data_size = len(data)
-					if data_size < 4096:
-						break
-
-					if response:
-						print(response)
-						data = input('>> ')
-						buffer += '\n'
-
+			self.socket.recv(4096)
+			ouput = executeCMD(command)
+			self.socket.send(output.encode())
 		except:
-			print('[-] Connection Terminated')
-			self.socket.close()
+			print('[-] Communications Error Occured')
 			sys.exit()
 
-	def handle(self, client):
-		if self
-
-
 def executeCMD(command):
-	command = command.strip()
-	if command == '':
-		return
-	return subprocess.check_output(shlex.split(command), stderr=subprocess.STDOUT).decode()
+	try:
+		command = command.strip()
+		if command == '':
+			return
+		return subprocess.check_output(shlex.split(command), stderr=subprocess.STDOUT).decode()
+	except:
+		print('[-] Failed to process command!')
+		sys.exit()
+
+def createSESH(target, port):
+	print('[+] Attempting to create Netcat Session!')
+	try:
+		ncSESH = compSystem(target, port)
+		print('[+] Succeeded in creating connection with host!')
+		return ncSESH
+	except:
+		print('[-] Failed to create connection with host!')
+
+def spawnShell(ncSESH):
+	command = ' '
+	print('[+] Spawning Shell!')
+	while True:
+		command = input('>>: ')
+		if command != '':
+			ncSESH.run(command)
+		else:
+			break
+	print('[+] Closing Shell!')
 
 def main():
+	print('[+] Loaded up Python Netcat! proceeding to interpret command ...')
 	command = optparse.OptionParser()
+	command.add_option('-t', action='store', dest='target', type='string', help='specify the target')
+	command.add_option('-p', action='store', dest='port', type='int', help='specify the port num')
+	options, args = command.parse_args()
+	target = options.target
+	port = options.port
+	print('[+] Data Processing Complete!')
+	ncSESH = createSESH(target, port)
+	spawnShell(ncSESH)
 
 if __name__ == '__main__':
 	main()
